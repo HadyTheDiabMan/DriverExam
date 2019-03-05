d5a370d37d066fddbf3bdf1fa2ff8b13d8fb155a
hdiab22 <hdiab22@HS-MATH2-17.gvsd.org>
2019-03-05 12:43:53 -0500

    almost done

--- src/DriverExam.java
+++ src/DriverExam.java
@@ -5,17 +5,85 @@
  */
 public class DriverExam 
 {
-private char[] correctAnswers= new char[20];
+private char[] correctAnswers;
 
-public DriverExam()
+
+public DriverExam(char[] correctAnsList)
 {
-	char[] correctAnswers= {'B','D','A','A','C','A','B','A','C','D','B','C','D','A','D','C','C','B','D','A'};
+	correctAnswers=correctAnsList;
+}
+//dont need to perm student answers. just compare.
+//public void boolean passed()
+
+public int totalCorrect(char[]studentAnswers)
+{
+	int numberCorrect=0;
+	for(int i=0;i<studentAnswers.length;i++)
+	{
+		if(studentAnswers[i]==correctAnswers[i])
+		{
+			numberCorrect++;
+		}
 		
+	}
+	return numberCorrect;
 }
 
-public void boolean passed()
+public int totalIncorrect(char[]studentAnswers)
+{
+	int numberIncorrect=0;
+	for(int i=0;i<studentAnswers.length;i++)
+	{
+		if(studentAnswers[i]!=correctAnswers[i])
+		{
+			numberIncorrect++;
+		}
+		
+	}
+	return numberIncorrect;
+}
 
 
+public boolean passed(char[]studentAnswers)
+{
+	int numberCorrect=0;
+	for(int i=0;i<studentAnswers.length;i++)
+	{
+		if(studentAnswers[i]==correctAnswers[i])
+		{
+			numberCorrect++;
+		}
+		
+	}
+	if(numberCorrect/correctAnswers.length<=0.75)
+	{
+		return false;
+	}
+	else
+		return true;
+}
+public int questionsMissed(char[]studentAnswers)
+{
+	int numberIncorrect=0;
+	int[]  questionIncorrect = new int[studentAnswers.length];
+	for(int i=0;i<studentAnswers.length;i++)
+	{
+		if(studentAnswers[i]!=correctAnswers[i])
+		{
+			
+			questionIncorrect[i]=i;
+			
+			return questionIncorrect[i++];
+		}
+		
+	}
+	
+}
+
+
+//call totalincorrect to establish length for questionsmissed
+//totalincorrect() (run method in same class)
+//(put inside declaration of array), or store into var
 	
 
 }
